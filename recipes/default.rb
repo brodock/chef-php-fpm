f940efc502ae347075ad4ec4e4cd662e2c147102
Gabriel Mazetto <brodock@gmail.com>
2014-05-07 14:15:21 -0300

    Don't need to override support on services

--- recipes/default.rb
+++ recipes/default.rb
@@ -52,7 +52,6 @@ end
 service "php-fpm" do
   provider service_provider if service_provider
   service_name php_fpm_service_name
-  supports :start => true, :stop => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
 
